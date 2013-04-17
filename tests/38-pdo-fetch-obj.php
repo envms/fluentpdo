30c5aa739b78e3af8a5f12f41e996419a8b6490a
Beau West <beau@beaudesigns.net>
2013-04-17 08:19:11 -0500

    Added a new test and fixed a couple other tests for consistency

---
+++ tests/38-pdo-fetch-obj.php
@@ -0,0 +1,28 @@
+--TEST--
+PDO::FETCH_OBJ option.
+--FILE--
+<?php
+include_once dirname(__FILE__) . "/connect.inc.php";
+/* @var $fpdo FluentPDO */
+
+
+$query = $fpdo->from('user')->where('id > ?', 0)->orderBy('name');
+$query = $query->where('name = ?', 'Marek');
+$fpdo->getPdo()->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+print_r($query->getParameters());
+print_r($query->fetch());
+?>
+--EXPECTF--
+Array
+(
+    [0] => 0
+    [1] => Marek
+)
+stdClass Object
+(
+    [id] => 1
+    [country_id] => 1
+    [type] => admin
+    [name] => Marek
+)
