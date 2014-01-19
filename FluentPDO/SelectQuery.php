c44bddbf69606a388fca81af3bef2dfe44c49ddc
CrazyPHP <peeplanet@gmail.com>
2014-01-19 15:39:17 +0400

    add count function

--- FluentPDO/SelectQuery.php
+++ FluentPDO/SelectQuery.php
@@ -128,6 +128,14 @@ class SelectQuery extends CommonQuery {
 			return false;
 		}
 	}
+    
+    /**
+    * Make current query counting rows
+    * @return int 
+    */
+    public function fetchCount(){
+        return $this->select(null)->limit(null)->orderBy(null)->select('COUNT(*)')->fetch('COUNT(*)');    
+    }
 
 }
 
