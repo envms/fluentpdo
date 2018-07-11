eddf39025a1b229c296404fa5329feda6eb6fbe7
cbornhoft <chris@env.ms>
2018-07-11 10:14:31 -0400

    Move old unit tests to `tests/legacy` Add first test using PHPUnit Add phpunit xml config Update phpunit version in composer Update travis php test script

---
+++ tests/StructureTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Envms\FluentPDO\Structure;
+
+/**
+ * Class StructureTest
+ *
+ * @covers \Envms\FluentPDO\Structure
+ */
+class StructureTest extends TestCase
+{
+
+    public function testBasicKey() {
+        $structure = new Structure();
+
+        self::assertEquals('id', $structure->getPrimaryKey('user'));
+        self::assertEquals('user_id', $structure->getForeignKey('user'));
+    }
+
+    public function testCustomKey() {
+        $structure = new Structure('whatAnId', '%s_\xid');
+
+        self::assertEquals('whatAnId', $structure->getPrimaryKey('user'));
+        self::assertEquals('user_\xid', $structure->getForeignKey('user'));
+    }
+
+    public function testMethodKey() {
+        $structure = new Structure('id', ['self', 'suffix']);
+
+        self::assertEquals('id', $structure->getPrimaryKey('user'));
+        self::assertEquals('user_id', $structure->getForeignKey('user'));
+    }
+
+    /**
+     * @param $table
+     *
+     * @return string
+     */
+    public static function suffix($table) {
+        return $table . '_id';
+    }
+
+}
